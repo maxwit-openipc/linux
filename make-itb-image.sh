8a76ed9d728960241d82fce15681ff04eeac173e
monkey <zxy267304@gmail.com>
2026-05-25 14:38:00 +0800

    Build itb image

---
+++ make-itb-image.sh
@@ -0,0 +1,3 @@
+# Run 'make ARCH=arm CROSS_COMPILE=xxx uImage' and 'make ARCH=arm CROSS_COMPILE=xxx dtbs' 
+# before build ITB image with 'mkimage'. 
+mkimage -f arch/arm/boot/image.its arch/arm/boot/image.itb
